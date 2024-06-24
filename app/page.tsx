4d356bf1806079687937236e999a983c1f26537e
norbertkross <norbertaberor@gmail.com>
2024-06-24 14:43:44 +0000

    fixed entry in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
+import TTS from "./text-to-speech/page";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        PLAY AUDIO FILES
-      </div>
+      <TTS />
     </main>
   );
 }
